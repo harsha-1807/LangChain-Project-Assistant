139f19911e5d86f74062bdbc48172104f44a2d86
harsha-1807 <harshavardhan.sr@kalvium.community>
2025-07-31 11:18:06 +0530

    frontend deployed and link added to cors

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -30,7 +30,8 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  
+    allow_origins=["http://localhost:3000",
+                   "https://lang-chain-project-assistant.vercel.app/",],  
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"],  
